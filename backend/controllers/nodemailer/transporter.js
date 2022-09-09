bfdb946e2fee698ff42bae4dcddf103852aa429c
Abhi673 <94429194+Abhi673@users.noreply.github.com>
2022-09-09 10:04:44 +0530

    Update transporter.js

--- backend/controllers/nodemailer/transporter.js
+++ backend/controllers/nodemailer/transporter.js
@@ -1,16 +1,23 @@
 import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+dotenv.config();
+// console.log(process.env.SMTP_SERVICE);
+// console.log(process.env.MAIL_PW);
+// console.log(process.env.MAIL_USER);
 const options = {
     // host: process.env.SMTP_HOST || 'smtp-relay.sendinblue.com',
     // port: process.env.SMTP_PORT || 587,
     // secure: false, // true for 465, false for other ports
-    service: process.env.SMTP_SERVICE,
+    
+    service:'gmail',
     auth: {
-        user: process.env.MAIL_USER || 'partheev8@gmail.com', // generated ethereal user
-        pass: process.env.MAIL_PW || 'OJAp1skFTG2gKZ9r', // generated ethereal password
+        user:process.env.MAIL_USER, // generated ethereal user
+        pass: process.env.MAIL_PW, // generated ethereal password
     },
     tls: {
         rejectUnauthorized: false,
     },
 }
+
 
 export const transporter = nodemailer.createTransport(options)
